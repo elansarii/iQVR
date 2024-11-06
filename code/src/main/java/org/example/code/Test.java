8e8a09a1efeacfcd093e1ea357431a740cfec261
ansari <elansari1211@gmail.com>
2024-11-06 19:15:43 +0300

    Created the test class

---
+++ code/src/main/java/org/example/code/Test.java
@@ -0,0 +1,7 @@
+package org.example.code;
+//This is temporary for testing ongoing methods
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
