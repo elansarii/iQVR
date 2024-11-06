c576c467e65dbee3add6bee4c19b98c693f3656d
ansari <elansari1211@gmail.com>
2024-11-06 19:18:52 +0300

    Tested the test class

--- code/src/main/java/org/example/code/Test.java
+++ code/src/main/java/org/example/code/Test.java
@@ -2,6 +2,6 @@ package org.example.code;
 //This is temporary for testing ongoing methods
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Test");
     }
 }
