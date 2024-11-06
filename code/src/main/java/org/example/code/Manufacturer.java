0971e3df94d5465df94e2d30a291abf172db03b5
ansari <elansari1211@gmail.com>
2024-11-06 19:23:13 +0300

    Created the manufacturer class

---
+++ code/src/main/java/org/example/code/Manufacturer.java
@@ -0,0 +1,14 @@
+package org.example.code;
+
+public class Manufacturer {
+    private String manufacturerName;
+    private String manufacturerCountry;
+
+
+    public Manufacturer(String manufacturerCountry, String manufacturerName) {
+        this.manufacturerCountry = manufacturerCountry;
+        this.manufacturerName = manufacturerName;
+    }
+
+
+}
