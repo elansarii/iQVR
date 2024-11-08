2486d5ee314b8f85ddd101dabf808e357e6e1cd9
ansari <elansari1211@gmail.com>
2024-11-09 00:07:25 +0300

    Added the code for redirecting the user to the choice view

--- code/src/main/java/org/example/code/LoginController.java
+++ code/src/main/java/org/example/code/LoginController.java
@@ -1,12 +1,29 @@
 package org.example.code;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static java.lang.System.exit;
 
 public class LoginController {
     public void loginUser(ActionEvent actionEvent) {
-        System.out.println("Admin logged in");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/code/choice-view.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Choice View");
+            stage.setScene(new Scene(root));
+            stage.show();
+            // Close the current window
+            ((Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow()).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void close(ActionEvent actionEvent) {
