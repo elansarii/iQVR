fe8a4fe98103f129193b05579bca74a6ff770c1d
ansari <elansari1211@gmail.com>
2024-11-08 20:28:18 +0300

    Renamed login view fxml file

--- code/src/main/java/org/example/code/HelloApplication.java
+++ code/src/main/java/org/example/code/HelloApplication.java
@@ -8,6 +8,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Objects;
+
 public class HelloApplication extends Application {
 
     private double x = 0;
@@ -15,7 +17,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login-view.fxml")));
 
         Scene scene = new Scene(root);
 
