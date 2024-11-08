46e922cd29075f3fef621e401096a6eeecc2c1b0
ansari <elansari1211@gmail.com>
2024-11-09 00:27:35 +0300

    Updated the styling and layout of login and choice views

--- code/src/main/java/org/example/code/ChoiceController.java
+++ code/src/main/java/org/example/code/ChoiceController.java
@@ -1,6 +1,15 @@
 package org.example.code;
 
+import javafx.event.ActionEvent;
+
 public class ChoiceController {
 
 
+    public void transferOwnership(ActionEvent actionEvent) {
+
+    }
+
+    public void reportAccident(ActionEvent actionEvent) {
+
+    }
 }
