57244c074f922542e1a7f66de5a61eb9976a2bd2
hm77a <ha2205541@qu.edu.qa>
2024-11-23 00:45:59 +0300

    deleted unnecessary methods

--- code/src/main/java/org/example/code/InsurancePolicy.java
+++ code/src/main/java/org/example/code/InsurancePolicy.java
@@ -25,18 +25,7 @@ public class InsurancePolicy {
         return !today.before(issueDate) && !today.after(expiryDate);
     }
 
-//    public String getInsurancePolicyDetails() {
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        return "Policy Number: " + policyNumber + "\n" +
-//                "Insurance Company: " + insuranceCompany + "\n" +
-//                "Issue Date: " + sdf.format(issueDate) + "\n" +
-//                "Expiry Date: " + sdf.format(expiryDate) + "\n" +
-//                "VIN: " + vin;
-//    }
 
-    public void updateInsurancePolicy() {
-
-    }
 
 
 }
